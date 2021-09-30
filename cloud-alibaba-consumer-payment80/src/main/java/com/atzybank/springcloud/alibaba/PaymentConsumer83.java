8d4b537209017642468efbf3734278b1ab640da9
陈利安 <lian.chen4@pactera.com>
2021-09-30 23:29:05 +0800

    nacos 集群配置

---
+++ cloud-alibaba-consumer-payment80/src/main/java/com/atzybank/springcloud/alibaba/PaymentConsumer83.java
@@ -0,0 +1,13 @@
+package com.atzybank.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentConsumer83 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentConsumer83.class, args);
+    }
+}
---
+++ cloud-alibaba-consumer-payment80/src/main/java/com/atzybank/springcloud/alibaba/controller/OrderPaymentController.java
@@ -0,0 +1,27 @@
+package com.atzybank.springcloud.alibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderPaymentController {
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverUrl;
+
+    @Resource
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return restTemplate.getForObject(serverUrl + "/payment/nacos/" + id, String.class);
+    }
+
+}
